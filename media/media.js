b66032826e5bc2e11ac298ba098692f0049d8ddd
jaseraso@gmail.com <jaseraso@gmail.com@54650146-de80-5f21-a844-70692b331f05>
2012-11-29 12:00:40 +0000

    68

--- media/media.js
+++ media/media.js
@@ -110,12 +110,13 @@ theText = theText.replace(/ز2/gi, "<img src='http://i45.tinypic.com/t9k5jr.jpg'
 theText = theText.replace(/ز3/gi, "<img src='http://i47.tinypic.com/2zeiex3.jpg'/>");
 theText = theText.replace(/ز4/gi, "<img src='http://i50.tinypic.com/aequx3.jpg'/>");
 theText = theText.replace(/ز5/gi, "<img src='http://i46.tinypic.com/2unwp09.jpg'/>");
-
+/* يد */
 theText = theText.replace(/ي1/gi, "<img src='http://i50.tinypic.com/qsjxqg.jpg'/>");
 theText = theText.replace(/ي2/gi, "<img src='http://i48.tinypic.com/k54u3b.jpg'/>");
-theText = theText.replace(/ي3/gi, "<img src='http://i47.tinypic.com/2zeiex3.jpg'/>");
-theText = theText.replace(/ي4/gi, "<img src='http://i49.tinypic.com/2uj2lfp.jpg'/>");
+theText = theText.replace(/ي3/gi, "<img src='http://i49.tinypic.com/2uj2lfp.jpg'/>");
+theText = theText.replace(/ي4/gi, "<img src='http://i45.tinypic.com/e9stxs.jpg'/>");
 theText = theText.replace(/ي5/gi, "<img src='http://i50.tinypic.com/15h1op0.jpg'/>");
+
 bodyText.innerHTML = theText;
 }replaceText();
 
